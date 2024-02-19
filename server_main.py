85be2ae360805e91c13824cb3106149319339bbf
Srishti <118805613+srishmath@users.noreply.github.com>
2024-02-19 22:21:05 +0530

    Update server_main.py

--- server_main.py
+++ server_main.py
@@ -1,21 +1,27 @@
 from socket import *
+import ssl
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print('The server is ready to receive')
 
+#SSL CONTEXT CREATION
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(r"C:\Users\srish\localhost.crt",r"C:\Users\srish\localhost.key")
+
+
 while True:
     connectionSocket, addr = serverSocket.accept()
-
+    ssock=context.wrap_socket(connectionSocket,server_side=True)
     message='select person 1:you,person 2:me'
-    connectionSocket.send(message.encode())
+    ssock.send(message.encode())
 
-    sentence = connectionSocket.recv(1024).decode()
-    # capitalizedSentence = sentence.upper()
-    # connectionSocket.send(capitalizedSentence.encode())
-    print("recived: ",sentence)
-    connectionSocket.close()
+    sentence = ssock.recv(1024).decode()
+    
+    print("recieved: ",sentence)
+    ssock.close()
 
     # # Choose a server to forward the connection to
     # server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
