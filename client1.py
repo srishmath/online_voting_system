803cee2ff03f44a5442fcda42eb0dfd956454084
Srishti <118805613+srishmath@users.noreply.github.com>
2024-02-19 22:20:34 +0530

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,14 +1,22 @@
 from socket import *
-serverName = '127.0.0.1'
+import ssl
+
+serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-# sentence = input('Input lowercase sentence:')
-# clientSocket.send(sentence.encode())
 
-Sentence_recv = clientSocket.recv(1024)
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context.load_verify_locations(r"C:\Users\srish\localhost.crt")
+
+# Wrap the socket object with the SSL context
+ssock = context.wrap_socket(clientSocket, server_hostname='localhost')
+
+ssock.connect((serverName,serverPort))
+
+
+Sentence_recv = ssock.recv(1024)
 print(Sentence_recv)
 num=input("enter no:")
 modifiedMessage="chose"+num
-clientSocket.send(modifiedMessage.encode())
-clientSocket.close()
+ssock.send(modifiedMessage.encode())
+ssock.close()
