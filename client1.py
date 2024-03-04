b8df160ced8317f23f4e82d97e13f20b1f406da6
Srishti <118805613+srishmath@users.noreply.github.com>
2024-03-04 12:22:09 +0530

    Update client1.py

--- client1.py
+++ client1.py
@@ -12,11 +12,23 @@ context.load_verify_locations(r"C:\Users\srish\localhost.crt")
 ssock = context.wrap_socket(clientSocket, server_hostname='localhost')
 
 ssock.connect((serverName,serverPort))
-
-
-Sentence_recv = ssock.recv(1024)
+# sentence = input('Input lowercase sentence:')
+# clientSocket.send(sentence.encode())
+print("hi")
+Sentence_recv = ssock.recv(1024).decode()
+print("hi")
 print(Sentence_recv)
 num=input("enter no:")
-modifiedMessage="chose"+num
+modifiedMessage=num
 ssock.send(modifiedMessage.encode())
+received_data=ssock.recv(1024)
+
+
+
+# Decode and print the statistics
+stats = received_data.decode().split(',')
+print("Received stats:")
+print("Stat 1:", stats[0])
+print("Stat 2:", stats[1])
+print("Stat 3:", stats[2])
 ssock.close()
