ef2f3b49d0d512670a23de240cb6c35c323cc2f8
suhan18 <134138944+suhan18@users.noreply.github.com>
2024-03-05 14:28:59 +0530

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,34 +1,52 @@
 from socket import *
 import ssl
+import threading
+import random
 
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
+def client_thread():
+    serverName = '192.168.96.67'
+    serverPort = 12000
+    
+    # Create a socket object for TCP/IP connection
+    clientSocket = socket(AF_INET, SOCK_STREAM)
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-context.load_verify_locations(r"C:\Users\srish\localhost.crt")
+    # Create SSL context and load certificate
+   
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT) #Transport Layer Security
+    context.check_hostname = False #not checking as hostname is statically assigned
+    context.verify_mode = ssl.CERT_NONE #NONE due to self signed certificate
+    
+    ssock = context.wrap_socket(clientSocket, server_hostname=serverName)
+    ssock.connect((serverName, serverPort))
+    
+   
+    
+    # Send and receive data
+    print("Connected to server")
+    Sentence_recv = ssock.recv(1024).decode()
+    print("Received from server:", Sentence_recv)
+    
+    # Generate a random number between 1, 2, or 3
+    num = str(random.randint(1, 3))
+    ssock.send(num.encode())
+    
+    received_data = ssock.recv(1024).decode()
+    print("Received from server:", received_data)
 
-# Wrap the socket object with the SSL context
-ssock = context.wrap_socket(clientSocket, server_hostname='localhost')
+    # Close the connection
+    ssock.close()
+    print("Connection closed")
 
-ssock.connect((serverName,serverPort))
-# sentence = input('Input lowercase sentence:')
-# clientSocket.send(sentence.encode())
-print("hi")
-Sentence_recv = ssock.recv(1024).decode()
-print("hi")
-print(Sentence_recv)
-num=input("enter no:")
-modifiedMessage=num
-ssock.send(modifiedMessage.encode())
-received_data=ssock.recv(1024)
+# Number of clients to create
+num_clients = 100
 
+# Create and start threads for each client
+threads = []
+for i in range(num_clients):
+    thread = threading.Thread(target=client_thread)
+    threads.append(thread)
+    thread.start()
 
-
-# Decode and print the statistics
-stats = received_data.decode().split(',')
-print("Received stats:")
-print("Stat 1:", stats[0])
-print("Stat 2:", stats[1])
-print("Stat 3:", stats[2])
-ssock.close()
+# Wait for all threads to finish
+for thread in threads:
+    thread.join()
